bb33478fa7c6d640e53b27136c63756957791966
joaoszs <joaolucio926@gmail.com>
2025-02-19 20:50:45 -0300

    Single-Item

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import SingleItem from './SingleItem';
 
 const ItemList = () => {
   return (
-    <div>ItemList</div>
+    <div className="item-list">
+
+        <div className="item-list__header">
+            <h2>Artistas populares</h2>
+            <a className="item-list__link" href="/">
+                Mostrar tudo
+            </a>
+        </div>
+    
+        <div className="item-list__container">
+            <SingleItem/>
+            <SingleItem/>
+            <SingleItem/>
+            <SingleItem/>
+            <SingleItem/>
+        </div>
+    </div>
   )
 }
 
